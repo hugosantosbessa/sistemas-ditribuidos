6825c2d7d47959b892299b0a04d53f277c3d91e1
Kassia Cristina de Sousa Lopes <kassialopes201@gmail.com>
2022-01-24 18:19:07 -0300

    commit

--- src/server/AddressBookEsqueleto.java
+++ src/server/AddressBookEsqueleto.java
@@ -16,6 +16,21 @@ public class AddressBookEsqueleto {
 		// (3) empacota resposta do m�todo servente e retorna
 		return null;
 	}
+	public byte[] CadastrarLivro(ByteString args) {
+		// (1) Desempacota argumento de entrada
+		// (2) chama o metodo do servente
+		// (3) empacota resposta do m�todo servente e retorna
+		return null;
+	}
+
+	public byte[] CadastrarAutor(ByteString args) {
+		// (1) Desempacota argumento de entrada
+		// (2) chama o metodo do servente
+		// (3) empacota resposta do m�todo servente e retorna
+		return null;
+	}
+
+
 
 	public byte[] list(ByteString args) {
 		// (1) Desempacota argumento de entrada
