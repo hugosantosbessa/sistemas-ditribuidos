c5ab40f4981d47b57dd22e221f2710f1e108421b
Kassia Cristina de Sousa Lopes <kassialopes201@gmail.com>
2022-01-25 21:01:52 -0300

    commit

--- src/client/AddressBookClient.java
+++ src/client/AddressBookClient.java
@@ -73,13 +73,106 @@ public class AddressBookClient {
 			break;
 			
 		case 2:
-			//proxy.CadastrarLivro(opt, opt, opt, opt, opt);
+			System.out.println("Digite o título do livro: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			String titulo = opt + ";";
+			
+			System.out.println("Digite o código do livro: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			String cod = opt + ";";
+			
+			System.out.println("Digite o gênero do livro: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			String genero = opt + ";";
+			
+			System.out.println("Digite o autor do livro: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			String autor = opt + ";";
+			
+			System.out.println("Digite a quantidade de exemplares do livro: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			String quant_exemplares = opt + ";";
+			System.out.println(proxy.CadastrarLivro(titulo, cod, genero, autor, quant_exemplares));
 			break;
+			
 		case 3:
-			//proxy.CadastrarAutor(opt, opt, opt, opt);
+			System.out.println("Digite o nome do autor: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			nome = opt + ";";
+			
+			System.out.println("Digite o Id do autor: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			Id = opt + ";";
+			
+			System.out.println("Digite o email do autor: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			email = opt + ";";
+			
+			System.out.println("Digite o CPF do autor: ");
+			do {
+				try {
+					opt = stdin.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} while (opt.equals("\n") || opt.equals("") || opt.isEmpty());
+			cpf = opt + ";";
+			System.out.println(proxy.CadastrarAutor(nome, Id, email, cpf)); 
 			break;
+<<<<<<< HEAD
 		case 4:
 			//proxy.RealizarEmprestimo(opt,opt,opt,opt,opt,opt)
+=======
+			
+>>>>>>> df7a7724f2ef53b1a23d8fcfc25b1b2bcbe693ca
 		default:
 			System.out.println("Operacao invalida, tente outra.");
 			break;
